69670707b2730ee722fe9da292159d6bd34b87b0
Paul Debarbieux <paul.debarbieux@edu.ece.fr>
2025-03-19 14:37:44 +0100

    Changement dans le lib_dataset pour que le fonctionnement soit sur google colab et écrit dans des .txt les dossiers à utiliser pour les images.

--- Codes_py/lib_dataset_path.py
+++ Codes_py/lib_dataset_path.py
@@ -2,49 +2,66 @@
 
 # Import des librairies
 
-import kagglehub
+from kaggle_datasets import KaggleDatasets
 import os
 
 # Constantes 
 
 # Fonctions/Procedures
 
-def kaggle_setup():
+def kaggle_setup(lib=2):
     # Procédure qui va récupérer le lien du dataset kaggle pour ce projet. 
-    path = kagglehub.dataset_download("ishandandekar/fruitimagedataset")
-    print(f"Adresse du dataset general: {path}")
+    if lib == 1:
+        path_lib = KaggleDatasets().get("moltean/fruits")
+    else
+        path_lib = KaggleDatasets().get("ishandandekar/fruitimagedataset")
+    print(f"Adresse du dataset choisi: {path}")
 
-    return path
+    return path_lib
 
-def fruit_path(source=1):
-    path=kaggle_setup
-    if source ==1:
-        path=path+"/test/test"
-    else:
-        path=path+"train/train"
+def path_to(lib=2,source=1):
+    path=kaggle_setup(lib)
+    match lib
+    case 1:
+        if source ==1:
+            path = path+"/fruits-360_100x100/fruits360/Test"
+        else 
+            path = path+"/fruits-360_100x100/fruits360/Train"
+    case _:
+        if source == 1:
+            path = path + "/test/test"
+        else:
+            path= path + "/train/train"
     
     assert os.path.exists(path),f"Le chemin trouve sur Kaggle est indisponible : {path}"
 
     apple_rep=[]
     pear_pre=[]
+    exclusions=["Apple Core 1","Apple Rotten 1","Apple hit 1","Pear Stone 1"]
+
     for dir in os.listdir(path):
         if os.path.isdir(os.path.join(path,dir)):
-            if dir.startswith("Apple"):
+
+            if dir.startswith("Apple") and dir not in exclusions :
                 apple_rep.append(f"/{dir}")
-            elif dir.startswith("Pear"):
+
+            elif dir.startswith("Pear") and dir not in exclusions :
                 pear_pre.append(f"/{dir}")
+
     with open("rep_apple.txt","w",encoding="utf-8") as apple_file:
         apple_file.write("\n".join(apple_rep))
+
     with open("rep_pear.txt","w",encoding="utf-8") as pear_file:
         pear_file.write("\n".join(pear_pre))
+
+    return path
 
 
 # Programme principal
 # Test de la création des .txt et de l'ananlyse du dataset
 if __name__=="__main__":
-    print("Debut du programme")
-
-# Download latest version
-path = kagglehub.dataset_download("ishandandekar/fruitimagedataset")
-
-print("Path to dataset files:", path)
+    print(f"Debut du programme")
+    choix_dataset = int(input("Valeur du dataset : 1 Fruit 360 | autre : Autre dataset"))
+    train_or_test = int(input("Valeur du jeu : 1= test | autre = train"))
+    path_to(choix_dataset,train_ot_test)
+    print(f"Fin du Test du programme")
